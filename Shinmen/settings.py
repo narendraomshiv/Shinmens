8dfe6c8a6886f04f3042d70641e82edbb13ec1e6
Abhishek Yadav <mobappssolutions147@gmail.com>
2022-09-14 16:47:11 +0530

    add

--- Shinmen/settings.py
+++ Shinmen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!3u+hj0vjgr%8=uypbutn7x@7$pubj^b33wxzqkswsygn#lba7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
